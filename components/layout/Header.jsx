0f4027ea341dd344a6c1f529b3aa428398eae819
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-09-10 23:40:08 -0500

    estilos con emotion, styled components

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,31 +1,94 @@
+/** @jsx jsx */
 import React from 'react';
+import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css, jsx } from '@emotion/core';
+
+
 import Buscar from '../ui/Buscar';
 import Navegacion from '../layout/Navegacion';
-import Link from 'next/link';
+import Boton from '../ui/Boton';
+
+
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%95%;
+    margin: 0 auto;
+    @media (min-width: 768px){
+        display: flex;
+        justify-content: space-between;
+    }
+`
+
+const Logo = styled.p`
+  color: var(--naranja);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: var(--roboto);
+  margin-right: 2rem;
+`;
+
 
 const Header = () => {
+
+    const usuario = true;
+
     return ( 
 
-        <header>
-            <div>
+        <header
+            css={css `
+                border-bottom: 2px solid var(--gris3);
+                padding: 1rem 0;
+            `}
+        >
+            <ContenedorHeader>
                 <div>
-                    <p>P</p>
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
 
                     <Buscar/>
 
                     <Navegacion/>
                 </div>
 
-                <div>
-                    <p>Hola: Alex</p>
-                    <button type="button">Cerrar Sesión</button>
+                <div
+                    css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
+
+                {usuario
+                    ?(<> 
+                        <p css={css` margin-right: 2rem; `}>
+                            Hola: Alex                    
+                        </p>
+                        <Boton bgColor="true">Cerrar Sesión</Boton>                            
+                    </>
+                    )
+
+                    :(<>
+                        <Link href="/">
+                            <Boton bgColor="true">
+                                Login
+                            </Boton>
+                        </Link>
+                        <Link href="/">
+                            <Boton>
+                                Crear Cuenta
+                            </Boton>
+                        </Link>
+                    </>
+                    )
+
+                }
                     
-                    <Link href="/">Login</Link>
-                    <Link href="/">Crear Cuenta</Link>
 
 
                 </div>
-            </div>
+            </ContenedorHeader>
         </header>
      );
 }
