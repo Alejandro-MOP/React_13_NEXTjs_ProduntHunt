bc315116daa3b75ee28820d2aaaa62d93348de74
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-11-10 20:17:49 -0600

    Custom Hook
    Se encarga de realizar validaciones en formularios, necesita un state, regla de validacion y una funcion a ejecutar si todo esta ok

---
+++ hooks/useValidacion.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+const useValidacion = ( stateInicial, validar, fn ) => {
+
+    const [valores, guardarValores] = useState(stateInicial);
+    const [errores, guardarErrores] = useState({});
+    const [submitForm, guardarSubmitForm] = useState(false);
+
+    useEffect(() => {
+        if(submitForm){
+            const noErrores = Object.keys(errores).length === 0;
+
+            if(noErrores) fn(); //funcion que se ejecuta en el componente
+
+            guardarSubmitForm(false);
+        }
+    }, [errores])
+
+    //fn que se ejecuta conforme el usuario escribe algo
+    const handleChange = e => {
+        guardarValores({
+            ...valores,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //fn que se ejecuta cuando se da el submit
+    const handleSubmit = e => {
+        e.preventDefault();
+        const erroresValidacion = validar(valores); //reglas de validación
+        guardarErrores(erroresValidacion);
+        guardarSubmitForm(true);
+    }
+
+
+    return {
+        valores,
+        errores,
+        submitForm,
+        handleChange,
+        handleSubmit
+    }
+}
+
+export default useValidacion;
