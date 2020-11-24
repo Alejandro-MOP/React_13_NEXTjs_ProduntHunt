53b952a4182adb000439a6f9582d97ee7db8aa40
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-11-23 21:44:27 -0600

    Configuración Firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDk4L4KbkYhXMYwRX1WuH30I1JIcXrSg0w",
+    authDomain: "product-hunt-f6519.firebaseapp.com",
+    databaseURL: "https://product-hunt-f6519.firebaseio.com",
+    projectId: "product-hunt-f6519",
+    storageBucket: "product-hunt-f6519.appspot.com",
+    messagingSenderId: "331303020172",
+    appId: "1:331303020172:web:562eac7d38041cf1b6fda9"
+};
+
+export default firebaseConfig;
