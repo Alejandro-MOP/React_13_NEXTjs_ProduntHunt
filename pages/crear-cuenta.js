0c3b9b6b83c55054450e01f349bca6398a6674bd
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-11-23 20:56:10 -0600

    View Crear Cuenta
    Escribir todo el jsx para la vista y consumi custom hook para realizar valdiaciones en el formulario

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,17 +1,96 @@
+/** @jsx jsx */
 import React from 'react';
+import { css, jsx } from '@emotion/core'
 import Layout from '../components/layout/Layout';
+import { Formulario, Campo, InputSubmit, Error } from '../components/ui/Fomulario'
 
+//customHooks
+import useValidacion from '../hooks/useValidacion';
+import validarCrearCuenta from '../validacion/validarCrearCuenta'
 
 const CrearCuenta = () => {
-  return (
 
-    <div>
-      <Layout>
-        <h1>Crear Cuenta</h1>
-      </Layout>
-    </div>
+    const STATE_INICIAL = {
+        nombre: '',
+        email: '',
+        password: ''
+    }
+    const crearCuenta = () => console.log('creando cuenta..')
 
-  )
+    const { valores, errores, handleChange, handleSubmit, handleBlur } = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
+
+    const { nombre, email, password } = valores;
+
+    return (
+
+        <div>
+            <Layout>
+                <>
+                    <h1
+                        css={css`
+                        text-align: center;
+                        margin-top: 5rem;
+                    `}>
+                        Crear Cuenta</h1>
+                    <Formulario
+                        onSubmit={handleSubmit}
+                        noValidate
+                    >
+                        <Campo>
+                            <label htmlFor="nombre">Nombre</label>
+                            <input
+                                type="text"
+                                id="nombre"
+                                placeholder="Tu Nombre"
+                                name="nombre"
+                                value={nombre}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                        </Campo>
+
+                        { errores.nombre && <Error>{ errores.nombre }</Error> }
+
+                        <Campo>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="Tu Email"
+                                name="email"
+                                value={email}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                        </Campo>
+
+                        { errores.email && <Error>{ errores.email }</Error> }
+
+                        <Campo>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Tu password"
+                                name="password"
+                                value={password}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                        </Campo>
+
+                        { errores.password && <Error>{ errores.password }</Error> }
+
+                        <InputSubmit
+                            type="submit"
+                            value="Crear Cuenta"
+                        />
+                    </Formulario>
+                </>
+            </Layout>
+        </div>
+
+    )
 }
 
 export default CrearCuenta
