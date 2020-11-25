fafb774900d8f65cb654ad4b56bf6db450d1bc97
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-11-24 21:25:52 -0600

    View Login (iniciar-sesion)
    Se crea todo el jsx similar a crear-cuenta pero con funcionalidad de iniciar sesion

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,98 @@
-import React from 'react';
+/** @jsx jsx */
+import React, { useState } from 'react';
+import { css, jsx } from '@emotion/core'
+import Router from 'next/router';
 import Layout from '../components/layout/Layout';
+import { Formulario, Campo, InputSubmit, Error } from '../components/ui/Fomulario'
+
+import firebase from '../firebase';
+
+//customHooks
+import useValidacion from '../hooks/useValidacion';
+import validarIniciarSesion from '../validacion/validarIniciarSesion'
+
+const STATE_INICIAL = {
+  email: '',
+  password: ''
+}
 
 
 const Login = () => {
+
+  const [error, guardarError] = useState(false);
+
+
+  const { valores, errores, handleChange, handleSubmit, handleBlur } = useValidacion(STATE_INICIAL, validarIniciarSesion, iniciarSesion);
+
+  const { email, password } = valores;
+
+  async function iniciarSesion() {
+      try {
+          await firebase.login(email, password);
+          Router.push('/');
+      } catch (error) {
+          console.error('Hubo un error al autenticar al usuario:', error.message);
+          guardarError(error.message);
+      }
+  }
+
+
   return (
 
-    <div>
-      <Layout>
-        <h1>Login</h1>
-      </Layout>
-    </div>
+      <div>
+          <Layout>
+              <>
+                  <h1
+                      css={css`
+                      text-align: center;
+                      margin-top: 5rem;
+                  `}>
+                      Iniciar Sesión</h1>
+                  <Formulario
+                      onSubmit={handleSubmit}
+                      noValidate
+                  >
+
+                      <Campo>
+                          <label htmlFor="email">Email</label>
+                          <input
+                              type="email"
+                              id="email"
+                              placeholder="Tu Email"
+                              name="email"
+                              value={email}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                          />
+                      </Campo>
+
+                      { errores.email && <Error>{ errores.email }</Error> }
+
+                      <Campo>
+                          <label htmlFor="password">Password</label>
+                          <input
+                              type="password"
+                              id="password"
+                              placeholder="Tu password"
+                              name="password"
+                              value={password}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                          />
+                      </Campo>
+
+                      { errores.password && <Error>{ errores.password }</Error> }
+
+                      { error && <Error>{ error }</Error>}
+
+                      <InputSubmit
+                          type="submit"
+                          value="Iniciar Sesión"
+                      />
+                  </Formulario>
+              </>
+          </Layout>
+      </div>
 
   )
 }
